ad2761b8d55bd04e15890e0b1797c56204848384
Adam Pietrzycki <adam@pietrzycki.com>
2019-06-30 02:15:37 +0100

    Added logging
    Signed-off-by: Adam Pietrzycki <adam@pietrzycki.com>

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"go.uber.org/zap"
+	"os"
+)
+
 func main() {
+
+	if os.Getenv("ENV") == "PRODUCTION" {
+		logger, _ := zap.NewProduction()
+		zap.ReplaceGlobals(logger)
+	} else {
+		logger, _ := zap.NewDevelopment()
+		zap.ReplaceGlobals(logger)
+	}
 
 }
